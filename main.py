5554baa1099c9bff94221439057cab442ed8df59
icecake0141 <daisukek@me.com>
2020-12-05 20:35:19 +0000

    Rewrite bot as GaE/Web/Event API version

--- main.py
+++ main.py
@@ -2,189 +2,89 @@
 # coding: utf-8
 
 # Linguafrancatto
-# Language translation bot powered by DeepL for Slack
+# Slack Language translation bot powered by DeepL 
 # icecake0141
 
-### import
-import os
-import logging
-import re
+# Reference
+# https://github.com/slackapi/bolt-python/blob/main/examples/google_app_engine/flask/main.py
+
 import json
-import time
-import pprint
+import logging
+import os
+import re
 import requests
-from flask import Flask
-from slack_sdk import WebClient
-from slackeventsapi import SlackEventAdapter
-from slack_sdk.errors import SlackApiError
+import time
+from flask import Flask, request
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
 
-#from slack_sdk.rtm import RTMClient
+###  global variables
+# Debug
+DEBUG = os.environ.get("DEBUG_MODE")
 
-# entrypoint
+# DeepL API
+url = "https://api.deepl.com/v2/translate"
+deepl_auth_key = os.environ.get("DEEPL_TOKEN")
+
+
+############ Initialization ############
+# Initializes your app with your bot token and signing secret
+bolt_app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
 app = Flask(__name__)
-#slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
+handler = SlackRequestHandler(bolt_app)
 
-# Initialize a Web API client
-slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+# Debug
+#logging.basicConfig(level=logging.DEBUG)
 
-client = WebClient()
-api_response = client.api_test()
+#@app.middleware  # or app.use(log_request)
+#def log_request(logger, body, next):
+    #logger.debug(body)
+    #return next()
 
-###
-### Functions
+############ END Initialization ############
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+############ TEST ############
 
-# handy time printer
-#def get_time():
-  #localtime = time.localtime()
-  #result = time.strftime("%I:%M:%S %p", localtime)
-  #return result
+############ END TEXT ############
 
-# report message to guardian
-#def report_to_guardian(text, payload):
+# Listens to incoming messages that contain "hello"
+@bolt_app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(text=f"Hey there <@{message['user']}>!")
 
-    #web_client = payload['web_client']
-    #channel_id = web_client.conversations_open(users=guardian_uid)['channel']['id']
+@bolt_app.message(":wave:")
+def say_hello(message, say):
+    user = message['user']
+    say(f"Hi there, <@{user}>!")
 
-    #web_client.chat_postMessage(
-        #channel = channel_id,
-        #text = text
-    #)
+@bolt_app.message("translate")
+def translate(message, say):
 
-###
-### RTM event functions
+    # Post DeepL API request
+    payload_req = {"auth_key":deepl_auth_key,"text":message['text'], "target_lang":"EN"}
+    r = requests.get(url, params=payload_req)
 
-# When an API request is accepted
-# Get and store bot's ID and name
-#@RTMClient.run_on(event="open")
-#def bot_initialization(**payload):
+    translated_text = json.loads(r.text)["translations"][0]["text"]
 
-    ## import event data
-    #data = payload['data']
+    say(f"{translated_text}")
 
-    #text = json.dumps(pprint.pformat(data))
-    #report_to_guardian(text, payload)
+    time.sleep(1)
 
-    #global myid
-    #myid = data['self']['id']
-    #global myname
-    #myname = data['self']['name']
+@bolt_app.message("")
+def noop():
+    return "", 200, {}
 
 
-# When connection to a slack space is established
-# Report to a guardian
-#@RTMClient.run_on(event="hello")
-#def report_status_after_connection(**payload):
-
-    #text = "Hello, " + myname + " is up and running. " + get_time()
-
-    #report_to_guardian(text, payload)
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
 
 
-# When a new message is posted
-#@RTMClient.run_on(event="message")
-#@slack_events_adapter.on("message")
-#def translate_message(**payload):
-
-    #client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-
-    #try:
-        #response = client.chat_postMessage(channel='#random', text="Hello world!")
-        #assert response["message"]["text"] == "Hello world!"
-    #except SlackApiError as e:
-        ## You will get a SlackApiError if "ok" is False
-        #assert e.response["ok"] is False
-        #assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-        #print(f"Got an error: {e.response['error']}")
-
-    ## import event data
-    #event = payload.get['event',{}]
-
-    ## Exit if the message is from any bots
-    #if 'subtype' in event:
-        #if event['subtype'] == 'bot_message':
-            #return
-
-    ## import slack API info
-    #web_client = payload['web_client']
-
-    #if re.search('にゃん',data['text']) :
-        #tr_to_lang = 'EN'
-    #elif re.search('Meow',data['text']) :
-        #tr_to_lang = 'JA'
-    #elif re.search('Miaou',data['text']) :
-        #tr_to_lang = 'JA'
-    #elif re.search('мяу',data['text']) :
-        #tr_to_lang = 'JA'
-    #else:
-        #return
-
-    ## Post DeepL API request
-    #payload_req = {"auth_key":deepl_auth_key,"text":data['text'], "target_lang":tr_to_lang}
-    #r = requests.get(url, params=payload_req)
-
-    ## debug
-    #text = json.dumps(pprint.pformat(r.text))
-    #report_to_guardian(text, payload)
-
-    #translated_text = json.loads(r.text)["translations"][0]["text"]
-    #original_message_id = payload['data']['client_msg_id']
-
-    ## post message to Slack
-    #channel_id = data['channel']
-    #user_id = data['user']
-    #user_name = web_client.users_info(
-        #user = user_id
-    #)['user']['name']
-
-    #message = 'In message ' + original_message_id + ', ' + user_name + ' said:\n'
-    #message += translated_text
-
-    #web_client.chat_postMessage(
-        #channel = channel_id,
-        #text = message
-    #)
-    #time.sleep(1)
-
-###
-### main
+# Start your app
 if __name__ == "__main__":
-
-    ###  global variables
-    # DeepL API
-    #url = "https://api.deepl.com/v2/translate"
-    #deepl_auth_key = os.environ["DEEPL_TOKEN"]
-
-    # Slack
-    #slack_token = os.environ["SLACK_BOT_TOKEN"]
-    #guardian_uid = os.environ["GUARDIAN_UID"]
-
-    # bot name/id
-    #myid = ""
-    #myname = ""
-
-    # module logging
-    # Logging package for Python. Based on PEP 282 and comments thereto in comp.lang.python.
-    # To use, simply 'import logging' and log away!
-    logger = logging.getLogger()
-
-    # Set the log level to DEBUG. This will increase verbosity of logging messages
-    logger.setLevel(logging.DEBUG)
-
-    # class StreamHandler(stream: Optional[IO[str]]=...)
-    # A handler class which writes logging records, appropriately formatted,
-    # to a stream. Note that this class does not close the stream, as sys.stdout
-    # or sys.stderr may be used.
-    logger.addHandler(logging.StreamHandler())
-
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
-    # Create a RTM client license
-#    rtm_client = RTMClient(token=slack_token)
-
-    # Establish a WebSocket connection with Slack.
-#    rtm_client.start()
+    app.run(debug=DEBUG, port=int(os.environ.get("PORT", 3000)))
